522e91572a09591846f73998cb484710a9f10a2a
alehosainim-afk <alehosaini.m@gmail.com>
2026-05-15 22:04:52 +0100

    Update index.js

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ app.use(express.json());
 
 const CATEGORY_NAME = 'Tickts';
 const MESSAGE = 'If you want to add credits, make sure to purchase them from https://chroto.mysellauth.com/';
-const SELLAUTH_API_KEY = '5767773|P19HX6ZTQxYfbBVRoohSxN2o94DoseBfoRkOGFxv22fc9d63';
-const VOUCH_CHANNEL_ID = '1502780794999930961';
+const SELLAUTH_API_KEY = process.env.SELLAUTH_API_KEY;
+const VOUCH_CHANNEL_ID = process.env.VOUCH_CHANNEL_ID;
 
 client.on('ready', () => {
   console.log(`Bot is online as ${client.user.tag}`);
