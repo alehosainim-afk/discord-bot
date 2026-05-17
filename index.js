18efa45a860210d0fa8348e08af0153ff2480f7e
alehosainim-afk <alehosaini.m@gmail.com>
2026-05-17 18:40:29 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+const express = require('express');
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.DirectMessages
+  ]
+});
+
+const app = express();
+
+const CATEGORY_NAME = 'Tickts';
+const MESSAGE = 'If you want to add credits, make sure to purchase them from https://chroto.mysellauth.com/';
+const PURCHASE_CHANNEL_ID = '1504492433621254186';
+const VOUCH_CHANNEL_ID = process.env.VOUCH_CHANNEL_ID;
+
+client.on('ready', () => {
+  console.log(`Bot is online as ${client.user.tag}`);
+});
+
+client.on('channelCreate', async (channel) => {
+  if (channel.parent && channel.parent.name === CATEGORY_NAME) {
+    await channel.send(MESSAGE);
+  }
+});
+
 client.on('messageCreate', async (message) => {
   if (message.channelId !== PURCHASE_CHANNEL_ID) return;
   if (!message.embeds || message.embeds.length === 0) return;
@@ -31,3 +60,6 @@ client.on('messageCreate', async (message) => {
     console.log('Could not send DM:', e.message);
   }
 });
+
+app.listen(process.env.PORT || 3000);
+client.login(process.env.TOKEN);
